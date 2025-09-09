cc94913be22c1bfed582fb4848bc40d77b6085da
Kaley <kaleidox@comroid.org>
2025-09-09 18:15:34 +0200

    fix uuid typing

--- src/main/java/com/ampznetwork/herobrine/model/entity/Player.java
+++ src/main/java/com/ampznetwork/herobrine/model/entity/Player.java
@@ -1,17 +1,23 @@
 package com.ampznetwork.herobrine.model.entity;
 
+import com.ampznetwork.libmod.api.model.convert.UuidVarchar36Converter;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Id;
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "libmod_players")
 public class Player {
-    @Id UUID id;
+    @Id @Convert(converter = UuidVarchar36Converter.class)
+    @Column(columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    protected UUID id = UUID.randomUUID();
+
     String name;
 
     public com.ampznetwork.libmod.api.entity.Player upgrade() {
