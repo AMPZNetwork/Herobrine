f848035904545383ce3029e1f895383473e91b38
Kaleidox <git@kaleidox.de>
2026-03-31 07:26:43 +0200

    this isnt very important so as a hotfix we remove it

--- src/main/java/com/ampznetwork/herobrine/repo/GameRepository.java
+++ src/main/java/com/ampznetwork/herobrine/repo/GameRepository.java
@@ -1,7 +1,6 @@
 package com.ampznetwork.herobrine.repo;
 
 import com.ampznetwork.herobrine.feature.games.model.Game;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,11 +8,11 @@ import java.util.Collection;
 
 @Repository
 public interface GameRepository extends CrudRepository<Game, String> {
-    @Query("""
+    /*@Query("""
             select game from Game game
-             join GuildGameFlaglist flags on flags.guildId = :guildId
+             join GameFlaglist flags on flags.guildId = :guildId
              inner join flags.games anyGame on (flags.type = 1 and anyGame.name = game.name)
                                             or (flags.type = 0 and anyGame.name != game.name)
-            """)
+            """) todo fixme asap */
     Collection<Game> findAllByGuildId(long guildId);
 }
