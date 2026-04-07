b077cdac3128b75ca147f57ec56a15fe7be9b16d
Kaleidox <git@kaleidox.de>
2026-04-07 17:41:29 +0200

    linked account web api

--- src/main/java/com/ampznetwork/herobrine/component/JacksonProvider.java
+++ src/main/java/com/ampznetwork/herobrine/component/JacksonProvider.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.StreamReadFeature;
 import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +22,6 @@ public class JacksonProvider {
                 .enable(JsonParser.Feature.ALLOW_YAML_COMMENTS)
                 .enable(JsonParser.Feature.ALLOW_SINGLE_QUOTES)
                 .enable(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES)
-                .enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION));
+                .enable(JsonParser.Feature.STRICT_DUPLICATE_DETECTION)).registerModule(new Jdk8Module()).registerModule(new JavaTimeModule());
     }
 }
--- src/main/java/com/ampznetwork/herobrine/repo/LinkedAccountRepository.java
+++ src/main/java/com/ampznetwork/herobrine/repo/LinkedAccountRepository.java
@@ -2,13 +2,29 @@ package com.ampznetwork.herobrine.repo;
 
 import com.ampznetwork.herobrine.feature.accountlink.model.entity.LinkedAccount;
 import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Controller
 @Repository
+@RequestMapping("/linked_account")
 public interface LinkedAccountRepository extends CrudRepository<LinkedAccount, @NotNull Long> {
-    Optional<LinkedAccount> findByMinecraftId(UUID minecraftId);
+    @NonNull
+    @Override
+    @ResponseBody
+    @GetMapping("/by_discord/{id}")
+    Optional<LinkedAccount> findById(@PathVariable("id") @NotNull Long aLong);
+
+    @ResponseBody
+    @GetMapping("/by_minecraft/{id}")
+    Optional<LinkedAccount> findByMinecraftId(@PathVariable("id") UUID minecraftId);
 }
